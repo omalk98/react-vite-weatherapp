6bb6999f652a0fa98d983d2c556d2137fcd475c1
Omar <77292466+omalk98@users.noreply.github.com>
2022-02-16 03:03:43 -0500

    minor adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/react-vite-weatherapp'>
       <Navigation style={{ zIndex : 1000000 }} cityId={recentCities} />
       <Routes style={{ zIndex : 0 }}>
         <Route exact path="/" element={<Navigate to={"/home"} replace />} />
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import svgr from '@honkhonk/vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-vite-weatherapp/',
   plugins: [svgr(), react()]
 })
